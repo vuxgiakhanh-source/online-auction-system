adec173f4194079fdcb970bac929cd35b727fd94
Vu Gia Khanh <vuxgiakhanh@gmail.com>
2026-05-11 22:39:47 +0700

    fix observer

--- auction-server/src/main/java/com/group13/auction/observer/SellerObserver.java
+++ auction-server/src/main/java/com/group13/auction/observer/SellerObserver.java
@@ -2,15 +2,11 @@ package com.group13.auction.observer;
 
 import com.group13.auction.model.user.NormalUser;
 import com.group13.auction.service.iservice.IRatingService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Observer dành cho Seller - nhận notify về phiên đấu giá của mình.
  */
 public class SellerObserver implements AuctionObserver {
-
-  private static final Logger log = LoggerFactory.getLogger(SellerObserver.class);
 
   private final NormalUser seller;
   private final IRatingService ratingService;
@@ -28,14 +24,18 @@ public class SellerObserver implements AuctionObserver {
 
   @Override
   public void onBidPlaced(AuctionEvent event) {
-    log.info("Seller observer received bid event: sellerId={}, username={}, eventType={}, auctionId={}, amount={}",
-            seller.getId(), seller.getUsername(), event.getEventType(),
-            event.getAuction().getId(), event.getBidAmount());
     switch (event.getEventType()) {
       case BID_PLACED:
+        System.out.printf("[THÔNG BÁO tới Seller %s] Bid mới: %d | Phiên: %s%n",
+                seller.getUsername(), event.getBidAmount(), event.getAuction().getId());
         break;
 
       case BID_RESERVE_NOT_MET:
+        System.out.printf(
+                "[THÔNG BÁO tới Seller %s] Bid %d chưa đạt reserve price (%d).%n",
+                seller.getUsername(),
+                event.getBidAmount(),
+                event.getAuction().getReservePrice());
         break;
 
       default:
@@ -45,11 +45,10 @@ public class SellerObserver implements AuctionObserver {
 
   @Override
   public void onAuctionEnded(AuctionEvent event) {
-    log.info("Seller observer received auction event: sellerId={}, username={}, eventType={}, auctionId={}, amount={}",
-            seller.getId(), seller.getUsername(), event.getEventType(),
-            event.getAuction().getId(), event.getBidAmount());
     switch (event.getEventType()) {
       case AUCTION_STARTED:
+        System.out.printf("[THÔNG BÁO tới Seller %s] Phiên đấu giá của bạn đã bắt đầu!%n",
+                seller.getUsername());
         // TODO: notificationDao.save()
         break;
 
@@ -61,33 +60,58 @@ public class SellerObserver implements AuctionObserver {
 
       case AUCTION_ENDED:
         if (event.getBidder() != null) {
+          System.out.printf("[THÔNG BÁO tới Seller %s] Phiên kết thúc. Winner: %s | Giá: %d. Chờ thanh toán.%n",
+                  seller.getUsername(),
+                  event.getBidder().getUsername(),
+                  event.getBidAmount());
           // TODO: notificationDao.save()
         }
         break;
 
       case AUCTION_NO_WINNER:
+        System.out.printf(
+                "[THÔNG BÁO tới Seller %s] Phiên kết thúc không có ai đặt giá."
+                        + " Phiên đã bị hủy.%n",
+                seller.getUsername());
         // TODO: notificationDao.save()
         break;
 
       case RESERVE_NOT_MET_CLOSED:
+        System.out.printf("[THÔNG BÁO tới Seller %s] Phiên \"%s\" kết thúc với mức giá cao nhất là %d nhưng chưa đạt mức giá tối thiểu của bạn. Phiên đã bị hủy.%n",
+                seller.getUsername(),
+                event.getAuction().getItem().getName(),
+                event.getBidAmount());
         // TODO: notificationDao.save()
         break;
 
       case PAYMENT_COMPLETED:
         // Thông báo seller đã bán thành công
+        System.out.printf("[THÔNG BÁO tới Seller %s] Sản phẩm \"%s\" đã được bán thành công với giá %d! Tiền đã được chuyển vào tài khoản (sau thuế).%n",
+                seller.getUsername(),
+                event.getAuction().getItem().getName(),
+                event.getBidAmount());
         ratingService.rewardSeller(seller);
         // TODO: notificationDao.save()
         break;
 
       case AUCTION_CANCELED:
+        System.out.printf("[THÔNG BÁO tới Seller %s] Phiên đấu giá đã bị hủy.%n",
+                seller.getUsername());
         // TODO: notificationDao.save()
         break;
 
       case SECOND_CHANCE_OFFERED:
+        System.out.printf(
+                "[THÔNG BÁO tới Seller %s] Winner không thanh toán —"
+                        + " hệ thống đang chào cơ hội mua thứ cấp cho người đặt giá tiếp theo.%n",
+                seller.getUsername());
         // TODO: notificationDao.save()
         break;
 
       case SELLER_CANCEL_REQUEST_ACCEPTED:
+        System.out.printf(
+                "[THÔNG BÁO tới Seller %s] Yêu cầu hủy phiên của bạn đã được chấp thuận.%n",
+                seller.getUsername());
         // TODO: notificationDao.save()
         break;
       default:
