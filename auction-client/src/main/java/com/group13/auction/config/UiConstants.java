931ea28f2aec0ae69add36f6673c37eb01cbe9be
bingbongg <tthaonhi0127@gmail.com>
2026-05-31 16:58:02 +0700

    feat(client): add scalable JavaFX window resizing

--- auction-client/src/main/java/com/group13/auction/config/UiConstants.java
+++ auction-client/src/main/java/com/group13/auction/config/UiConstants.java
@@ -5,10 +5,13 @@ public final class UiConstants {
 
   public static final String APP_TITLE = "OmniBid - Online Auction System";
 
-  public static final double MIN_WIDTH = 1280.0;
-  public static final double MIN_HEIGHT = 720.0;
-  public static final double DEFAULT_WIDTH = 1280.0;
-  public static final double DEFAULT_HEIGHT = 720.0;
+  public static final double BASE_WIDTH = 1280.0;
+  public static final double BASE_HEIGHT = 720.0;
+  public static final double MIN_SCALE = 0.5;
+  public static final double MIN_WIDTH = BASE_WIDTH * MIN_SCALE;
+  public static final double MIN_HEIGHT = BASE_HEIGHT * MIN_SCALE;
+  public static final double DEFAULT_WIDTH = BASE_WIDTH;
+  public static final double DEFAULT_HEIGHT = BASE_HEIGHT;
 
   public static final String DEFAULT_FONT_FAMILY = "Montserrat";
   public static final String SIGNATURE_TEXT = "by CKNT team";
--- auction-client/src/main/java/com/group13/auction/ui/util/StageUtil.java
+++ auction-client/src/main/java/com/group13/auction/ui/util/StageUtil.java
@@ -19,6 +19,7 @@ public final class StageUtil {
   public static void configurePrimaryStage(Stage stage) {
     Objects.requireNonNull(stage, "stage must not be null");
     stage.setTitle(UiConstants.APP_TITLE);
+    stage.setResizable(true);
     stage.setMinWidth(UiConstants.MIN_WIDTH);
     stage.setMinHeight(UiConstants.MIN_HEIGHT);
     stage.setWidth(UiConstants.DEFAULT_WIDTH);
